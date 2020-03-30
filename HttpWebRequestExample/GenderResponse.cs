2f6567d1a6e8e6fb480d072a85fdd8c8c0d1c158
Vlad Snow <vlad.snow@bbconsult.co.uk>
2020-03-30 20:56:07 +0300

    Initilize project

---
+++ HttpWebRequestExample/GenderResponse.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpWebRequestExample
+{
+    public class GenderResponse
+    {
+        [JsonProperty("name")]
+        public string NameOfPerson { get; set; }
+
+        [JsonProperty("gender")]
+        public string Gender { get; set; }
+
+        [JsonProperty("probability")]
+        public double Probability { get; set; }
+
+        public int Count { get; set; }
+    }
+}
